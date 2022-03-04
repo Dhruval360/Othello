862c52b3607729986ddc2ed4fdc02b1df0a2d099
Dhruval360 <dhruvalpb@gmail.com>
2022-03-05 01:27:30 +0530

    Board design complete with circular coins.

--- app/src/main/java/com/example/othello/MainActivity.java
+++ app/src/main/java/com/example/othello/MainActivity.java
@@ -1,25 +1,18 @@
 package com.example.othello;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     final private Button[][] buttons = new Button[8][8];
-
+    final private boolean[][] buttonSet = new boolean[8][8];
     private boolean player1Turn = true;
 
-    private int roundCount;
-
-    private int player1Points;
-    private int player2Points;
-
+    // Use these to show the current scores
     private TextView textViewPlayer1;
     private TextView textViewPlayer2;
 
@@ -27,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Prevent screen rotation
 
         textViewPlayer1 = findViewById(R.id.text_view_p1);
         textViewPlayer2 = findViewById(R.id.text_view_p2);
@@ -37,7 +31,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
                 buttons[i][j] = findViewById(resID);
                 buttons[i][j].setOnClickListener(this);
-                buttons[i][j].setBackgroundColor(Color.parseColor("#185915"));
+                buttons[i][j].setBackgroundResource(R.drawable.empty_cell);
+                buttonSet[i][j] = false; // Meaning, this button hasn't been set yet
             }
         }
 
@@ -45,10 +40,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonReset.setOnClickListener(v -> {
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
-                    String buttonID = "button_" + i + j;
-                    int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
-                    buttons[i][j] = findViewById(resID);
-                    buttons[i][j].setBackgroundColor(Color.parseColor("#185915"));
+                    buttons[i][j].setBackgroundResource(R.drawable.empty_cell);
+                    buttonSet[i][j] = false; // This button hasn't been set yet
                 }
             }
         });
@@ -56,74 +49,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (!((Button) v).getText().toString().equals("")) {
-            return;
-        }
-
-        if (player1Turn) {
-            v.setBackgroundColor(Color.BLACK);
-        } else {
-            v.setBackgroundColor(Color.WHITE);
-        }
-
-        roundCount++;
-
-        if (checkForWin()) {
-            if (player1Turn) {
-                player1Wins();
-            } else {
-                player2Wins();
-            }
-        } else if (roundCount == 9) {
-            draw();
-        } else {
-            player1Turn = !player1Turn;
-        }
-
-    }
-
-    private boolean checkForWin() {
-        String[][] field = new String[3][3];
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                field[i][j] = buttons[i][j].getText().toString();
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            if (field[i][0].equals(field[i][1])
-                    && field[i][0].equals(field[i][2])
-                    && !field[i][0].equals("")) {
-                return true;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            if (field[0][i].equals(field[1][i])
-                    && field[0][i].equals(field[2][i])
-                    && !field[0][i].equals("")) {
-                return true;
-            }
-        }
-
-        if (field[0][0].equals(field[1][1])
-                && field[0][0].equals(field[2][2])
-                && !field[0][0].equals("")) {
-            return true;
-        }
-
-        return field[0][2].equals(field[1][1])
-                && field[0][2].equals(field[2][0])
-                && !field[0][2].equals("");
-    }
-
-    private void player1Wins() {
-    }
-
-    private void player2Wins() {
-    }
-
-    private void draw() {
+        final String tag = ((Button) v).getTag().toString();
+        final int pos = Integer.parseInt(tag);
+        if (buttonSet[pos / 10][pos % 10]) return;
+        if (player1Turn) v.setBackgroundResource(R.drawable.black_circle);
+        else v.setBackgroundResource(R.drawable.white_circle);
+        buttonSet[pos / 10][pos % 10] = true; // This button's state has been set and cannot be changed henceforth
+        player1Turn = !player1Turn;
     }
 }
