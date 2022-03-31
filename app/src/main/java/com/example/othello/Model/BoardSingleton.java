df7bba356d8b9b38d5a1158b6910aed0123b7077
Harsh188 <hiharshith18@gmail.com>
2022-03-31 14:09:32 +0530

    Added Singleton class and GameModel under Model directory

---
+++ app/src/main/java/com/example/othello/Model/BoardSingleton.java
@@ -0,0 +1,28 @@
+package com.example.othello.Model;
+
+import android.widget.Button;
+
+import com.example.othello.OthelloCell;
+
+public class BoardSingleton {
+    // Class object
+    private static BoardSingleton boardObj = new BoardSingleton();
+
+    // Game board attributes
+    final private Button[][] buttons = new Button[8][8];
+    private OthelloCell[][] board;
+    private boolean player1Turn;
+
+    private BoardSingleton(){
+
+    }
+
+    public static BoardSingleton getInstance(){
+        if(boardObj==null){
+            boardObj = new BoardSingleton();
+        }
+
+        return boardObj;
+    }
+
+}
