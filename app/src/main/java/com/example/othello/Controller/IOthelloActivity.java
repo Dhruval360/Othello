393198f0d5b4f28a633f0837419bc8263b45fb11
Dhruval360 <dhruvalpb@gmail.com>
2022-04-04 23:36:34 +0530

    Deleted Unused Files

--- app/src/main/java/com/example/othello/Controller/IOthelloActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.othello.Controller;
-
-public interface IOthelloActivity {
-    //TODO:
-    // 1. Choose settings
-    //  1.1 Call the model and retrieve player mode
-    // 2. Start up game
-    //  2.2
-}
